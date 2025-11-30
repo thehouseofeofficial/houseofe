ee4b2e1058cbc2d303f703589207d685338fff16
thehouseofeofficial <thehouseofeofficial@gmail.com>
2025-11-30 23:37:44 +0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- Show your Landing Page by default -->
+  <LandingPage />
+</template>
+
+<script setup>
+import LandingPage from './pages/LandingPage.vue'
+</script>
+
+<style>
+html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+</style>
